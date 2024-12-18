23d4beefb0ba736fcf06ad01dce6d3965d0d0ca7
Lucas Henrique <lucaz.hrq@gmail.com>
2024-12-17 23:14:03 -0300

    Criação Login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../styles/Login/LoginPage.css"; 
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -12,28 +13,38 @@ const LoginPage = () => {
 
   return (
     <div className="login-page">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+      <div className="login-container">
+        <h2>SGMI - Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Digite seu email"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label>Senha:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Digite sua senha"
+              required
+            />
+          </div>
+          <button type="submit" className="login-button">
+            Entrar
+          </button>
+        </form>
+        <div className="recovery-text">
+          Para recuperar ou mudar sua senha, entre em contato com o{" "}
+          <span>administrador do sistema</span>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+      </div>
     </div>
   );
 };
